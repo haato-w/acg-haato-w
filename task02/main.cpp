06e0de293cd15eecfc099c0da756e9ba701219ca
HaatoWatanabe <heart.watanabe2000@gmail.com>
2024-04-25 14:49:18 +0900

    task02 finished

--- task02/main.cpp
+++ task02/main.cpp
@@ -49,6 +49,45 @@ int number_of_intersection_ray_against_edge(
   //if (a * b > 0.f && d * c > 0.f && fabs(d) > fabs(c)) { return 1; }
 }
 
+float calc_theta(
+  const Eigen::Vector2f &p0,
+  const Eigen::Vector2f &p1)
+{
+  auto p0x = p0[0];
+  auto p0y = p0[1];
+  auto p1x = p1[0];
+  auto p1y = p1[1];
+
+  float p0_size = std::sqrt(p0x * p0x + p0y * p0y);
+  float p1_size = std::sqrt(p1x * p1x + p1y * p1y);
+  float dot = p0x * p1x + p0y * p1y;
+  float cos = dot / (p0_size * p1_size);
+  float cross = p0y * p1x - p0x * p1y;
+  float sin = cross / (p0_size * p1_size);
+  float theta = std::atan2(sin, cos);
+
+  return theta;
+}
+
+bool is_in_triangle(
+  const Eigen::Vector2f &ps,
+  const Eigen::Vector2f &pc,
+  const Eigen::Vector2f &pe,
+  const Eigen::Vector2f &q)
+{
+  float winding_number = 0;
+  winding_number += calc_theta(ps - q, pc - q);
+  winding_number += calc_theta(pc - q, pe - q);
+  winding_number += calc_theta(pe - q, ps - q);
+  const double PI = acos(-1);
+  winding_number /= 2 * PI;
+  const int int_winding_number = int(std::round(winding_number));
+  if (int_winding_number == 1)
+    // std::cout << "flag4" << std::endl;
+    return true;
+  return false;
+}
+
 /***
  *
  * @param org ray origin
@@ -65,8 +104,39 @@ int number_of_intersection_ray_against_quadratic_bezier(
     const Eigen::Vector2f &pc,
     const Eigen::Vector2f &pe) {
   // comment out below to do the assignment
-  return number_of_intersection_ray_against_edge(org, dir, ps, pe);
+  // return number_of_intersection_ray_against_edge(org, dir, ps, pe);
   // write some code below to find the intersection between ray and the quadratic
+  
+
+  // express ray as (Ax + By + C = 0)
+  const auto a = org;
+  const auto b = org + dir;
+  const double A = b[1] - a[1];
+  const double B = a[0] - b[0];
+  const double C = - b[1] * a[0] + b[0] * a[1];
+  
+  // tの2次方程式の係数
+  const double quad_A = (ps[0] - 2 * pc[0] + pe[0]) * A + (ps[1] - 2 * pc[1] + pe[1]) * B;
+  const double quad_B = 2 * (-ps[0] + pc[0]) * A + 2 * (-ps[1] + pc[1]) * B;
+  const double quad_C = ps[0] * A + ps[1] * B + C;
+
+  // 判別式を解く
+  const auto discriminant = quad_B * quad_B - 4 * quad_A * quad_C;
+  if (discriminant < 0)
+    return 0;
+  const auto t0 = (-quad_B + sqrt(quad_B * quad_B - 4 * quad_A * quad_C)) / (2 * quad_A);
+  const auto t1 = (-quad_B - sqrt(quad_B * quad_B - 4 * quad_A * quad_C)) / (2 * quad_A);
+  // 0 < t < 1
+  if ((!isfinite(t0) || t0 <= 0 || 1 <= t0) && (!isfinite(t1) || t1 <= 0 || 1 <= t1))
+    return 0;
+  const auto t = (!isfinite(t0) || t0 <= 0 || 1 <= t0) ? t1 : t0;
+  // qがps, pc, peの三角形の内部に存在するか s < 0対策
+  const auto q = (1 - t) * (1 - t) * ps + 2 * t * (1 - t) * pc + t * t * pe;
+  if (!is_in_triangle(ps, pc, pe, q)){
+    // std::cout << "not in triangle" << std::endl; // for debug
+    return 0;
+  }
+  return 1;
 }
 
 int main() {
